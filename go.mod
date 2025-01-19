51539929ef18147cdef6064a123d3b5447f64468
tarunsharma <tarun.kr.sharma@hotmail.com>
2025-01-19 13:56:50 +0400

    feat(service-discovery): added membership code using serf liberary

--- go.mod
+++ go.mod
@@ -7,6 +7,9 @@ toolchain go1.22.6
 require github.com/test-go/testify v1.1.4
 
 require (
+	github.com/hashicorp/serf v0.10.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
---
+++ internal/discovery/membership.go
@@ -0,0 +1,122 @@
+package discovery
+
+import (
+	"net"
+
+	"github.com/hashicorp/serf/serf"
+	"go.uber.org/zap"
+)
+
+type Membership struct {
+	Config
+	handler Handler
+	serf    *serf.Serf
+	events  chan serf.Event
+	logger  *zap.Logger
+}
+
+func New(handler Handler, config Config) (*Membership, error) {
+	m := &Membership{
+		Config:  config,
+		handler: handler,
+		events:  make(chan serf.Event),
+		logger:  zap.L().Named("membership"),
+	}
+
+	if err := m.setupSerf(); err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+type Config struct {
+	NodeName       string
+	BindAddr       string
+	Tags           map[string]string
+	StartJoinAddrs []string
+}
+
+func (m *Membership) setupSerf() error {
+	addr, err := net.ResolveTCPAddr("tcp", m.BindAddr)
+	if err != nil {
+		return err
+	}
+	config := serf.DefaultConfig()
+	config.Init()
+	config.MemberlistConfig.BindAddr = addr.IP.String()
+	config.MemberlistConfig.BindPort = addr.Port
+	m.events = make(chan serf.Event)
+	config.EventCh = m.events
+	config.NodeName = m.NodeName
+	config.Tags = m.Tags
+
+	serf, err := serf.Create(config)
+	if err != nil {
+		return err
+	}
+
+	m.serf = serf
+
+	go m.eventHandler()
+	if m.StartJoinAddrs != nil {
+		_, err := m.serf.Join(m.StartJoinAddrs, true)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type Handler interface {
+	Join(name, addr string) error
+	Leave(name string) error
+}
+
+func (m *Membership) eventHandler() {
+	for e := range m.events {
+		switch e.EventType() {
+		case serf.EventMemberJoin:
+			for _, member := range e.(serf.MemberEvent).Members { // e.(serf.MemberEvent) ??
+				if m.isLocal(member) {
+					continue
+				}
+				m.handleJoin(member)
+			}
+		case serf.EventMemberLeave, serf.EventMemberFailed:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					continue
+				}
+				m.handleLeave(member)
+			}
+		}
+	}
+}
+
+func (m *Membership) handleJoin(member serf.Member) {
+	m.logger.Info("Node joined", zap.String("name", member.Name), zap.String("addr", member.Addr.String()))
+	if err := m.handler.Join(member.Name, member.Tags["rpc_addrs"]); err != nil {
+		m.logger.Error("Failed to handle join", zap.Error(err))
+	}
+}
+
+func (m *Membership) handleLeave(member serf.Member) {
+	m.logger.Info("Node left", zap.String("name", member.Name), zap.String("addr", member.Addr.String()))
+	if err := m.handler.Leave(member.Name); err != nil {
+		m.logger.Error("Failed to handle leave", zap.Error(err))
+	}
+}
+
+func (m *Membership) isLocal(member serf.Member) bool {
+	return member.Name == m.serf.LocalMember().Name
+}
+
+func (m *Membership) Members() []serf.Member {
+	return m.serf.Members()
+}
+
+func (m *Membership) Leave() error {
+	return m.serf.Leave()
+}
